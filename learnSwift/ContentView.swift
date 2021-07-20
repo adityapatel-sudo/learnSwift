c9b9570af38a6f97d255f5cb401f14281410582e
adityapatel-sudo <adityapatel1204@gmail.com>
2021-07-19 22:56:44 -0700

    Initial commit

---
+++ learnSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  learnSwift
+//
+//  Created by Aditya Patel on 19/07/21.
+//  Copyright © 2021 AdityaPatel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
